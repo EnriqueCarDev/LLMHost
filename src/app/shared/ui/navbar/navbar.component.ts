ee2fddc496db8a2e2c9a63298b6b3f98a1d77904
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-04-27 23:45:38 +0200

    MUI installation

--- src/app/shared/ui/navbar/navbar.component.ts
+++ src/app/shared/ui/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isMobile: boolean;
 
+  constructor() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth < 768;
+    });
+  }
 }
