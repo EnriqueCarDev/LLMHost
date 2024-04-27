b0b418ba49a6a4372f164c53ed2d6b5574e900ae
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-04-27 23:08:39 +0200

    hero-button

--- src/app/home/sections/hero/hero.component.ts
+++ src/app/home/sections/hero/hero.component.ts
@@ -1,13 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/shared/ui/button/button.component';
 // import { hero_picture } from '../../../../assets/images';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
-  // hero_picture = hero_picture;
-}
+export class HeroComponent {}
--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ButtonComponent {
-
+  @Input() label: string = 'button';
+  @Output() buttonClick = new EventEmitter<void>();
 }
